b2e4360fa20bdc603a1a3175d71ed155e5de566d
Funkinsz <fl.crespel@gmail.com>
2023-04-26 15:50:47 +0200

    user verify

--- index.js
+++ index.js
@@ -33,8 +33,29 @@ app.post("/verifyUser", (req, res) => {
     const sql = `SELECT email FROM user WHERE email = "${email}"`
 
     connection.query(sql, (err, result) => {
+        console.log(result.length);
         if (err) throw err;
-        console.log("Utilisateur déja existant");
+        if (result.length > 0) {
+            console.log("Utilisateur déja existant");
+            res.send(JSON.stringify("user exist"))   
+        } else {
+            console.log("user is null");
+            res.send(JSON.stringify("User is null"))
+        }
+    })
+})
+
+// LOGIN USER
+app.post("/logUser", (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const sql = `SELECT email, password FROM user WHERE email = "${email}" AND password = "${password}"`
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("Connexion Success");
+        res.send(JSON.stringify("Coonect Success"))
     })
 })
 
@@ -50,24 +71,25 @@ app.post("/addUser", (req, res)  => {
     const date = req.body.date
     const status = req.body.status
 
-    const sql = `SELECT email FROM user WHERE email = "${email}"`
+    // const sql = `SELECT email FROM user WHERE email = "${email}"`
 
-    connection.query(sql, (err, result) => {
-        if (err) throw err;
-        if (result.length < 0) {
+    // connection.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     if (result.length > 0) {
+    //         console.log("Utilisateur déja existant");
+    //         res.send(JSON.stringify("Utilisateur déja existant"))
+    //     } else {
             const sql = `INSERT INTO user(surname, name, firstname, email, password, born, user_type, city, travel_time) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`
-            const values = [surname, name, firstname, email, password, city, travel,date, status]
+            const values = [surname, name, firstname, email, password, city, travel, date, status]
         
             connection.query(sql, values, (err, result) => {
                 if (err) throw err;
                 console.log("Utilisateur ajoutée en BDD");
+                res.send(JSON.stringify("User is add"))
             })
-        } else {
-            console.log("Utilisateur déja existant");
-            res.send(JSON.stringify("Utilisateur déja existant"))
-        }
+        // }
     })
-})
+// })
 
 app.listen(port, () => {
     console.log(`Serveur NodeJS écoutant sur le port ${port}`);
