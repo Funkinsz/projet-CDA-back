e414894049e0bde64465b5df49597ef367dcd096
Funkinsz <fl.crespel@gmail.com>
2023-05-11 17:11:33 +0200

    test de merde

--- index.js
+++ index.js
@@ -8,6 +8,53 @@ const routes = require("./routes");
 
 const app = express();
 
+const multer = require("multer");
+const upload = multer({
+  dest: "image",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+      cb(new Error("Please uplaod an image."));
+    }
+    cb(undefined, true);
+  },
+});
+
+image: {
+  type: Buffer;
+}
+
+app.post(
+  "/uplaod",
+  upload.single("upload"),
+  async (req, res) => {
+    try {
+      const incident = await Incident.findById(req.body.id);
+      incident.image = req.file.buffer;
+      incident.save();
+      res.send;
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+app.delete("/upload", async (req, res) => {
+  try {
+    const incident = await Incident.findById(req.body.id);
+    incident.image = undefined;
+    incident.save();
+    res.send();
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.use(cookie());
 
 app.use(bodyParser.json());
