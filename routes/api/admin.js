337b8f6da5101aa877f802b7802c3e999e2c26f4
Funkinsz <fl.crespel@gmail.com>
2023-06-13 12:36:38 +0200

    fix: update + delete add

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -20,7 +20,7 @@ router.post("/", async (req, res) => {
           if (bcrypt.compareSync(password, user.password)) {
             const token = jsonwebtoken.sign({}, key, {
               subject: user.id_user.toString(),
-              expiresIn: 3600 * 24,
+              expiresIn: 3600 * 4,
               algorithm: "RS256",
             });
             res.cookie("prestaTokenAdmin", token);
@@ -40,17 +40,19 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get('/current', async (req, res) =>{
-  const { prestaTokenAdmin } = req.cookies
+router.get("/current", async (req, res) => {
+  const { prestaTokenAdmin } = req.cookies;
 
+  console.log(prestaTokenAdmin);
   if (prestaTokenAdmin) {
     try {
       const decodedToken = jsonwebtoken.verify(prestaTokenAdmin, keyPub);
       console.log({ decodedToken });
-      const sql = `SELECT id_user, surname, name, firstname, email, born, profile_picture, banner_user, singer, city, travel_time, adress, desc_user, siret, user_role FROM user WHERE id_user = ${decodedToken.sub}`;
+      const sql = `SELECT id_user, email, user_role FROM user WHERE id_user = ${decodedToken.sub}`;
       connection.query(sql, (err, result) => {
         if (err) throw err;
         const currentUser = result[0];
+        console.log(currentUser);
         if (currentUser) {
           res.send(currentUser);
         } else {
@@ -61,9 +63,10 @@ router.get('/current', async (req, res) =>{
       res.send(JSON.stringify(null));
     }
   } else {
+    console.log("test");
     res.send(JSON.stringify(null));
   }
-})
+});
 
 // USERS
 router.get("/getUsers", (req, res) => {
@@ -76,8 +79,6 @@ router.get("/getUsers", (req, res) => {
 router.post("/updateUser", (req, res) => {
   const { id_user, surname, email, name, firstname, type } = req.body;
 
-  console.log({ id_user, surname, email, name, firstname, type });
-
   const sql = `UPDATE user SET surname = ?, email = ?, name = ?, firstname = ?, user_type = ? WHERE id_user = ?`;
   const values = [surname, email, name, firstname, type, id_user];
 
@@ -89,34 +90,73 @@ router.post("/updateUser", (req, res) => {
 });
 
 router.get("/banUser", (req, res) => {
-  const id = req.query.id
-  const role = req.query.role
+  const id = req.query.id;
+  const role = req.query.role;
   console.log(role);
 
-  let newRole
+  let newRole;
   if (role >= 0) {
-    newRole = -1
+    newRole = -1;
   } else {
-    newRole = 0
+    newRole = 0;
   }
 
   console.log(newRole);
 
-  const sql = `UPDATE user SET user_role = ? WHERE id_user = ?`
-  const value = [newRole, id]
+  const sql = `UPDATE user SET user_role = ? WHERE id_user = ?`;
+  const value = [newRole, id];
 
   connection.query(sql, value, (err, result) => {
     if (err) throw err;
     console.log("User Banned");
-    res.send(JSON.stringify(result))
-  })
-})
+    res.send(JSON.stringify(result));
+  });
+});
 
 // ADDS
 router.get("/getAdds", (req, res) => {
   connection.query("SELECT * FROM ad_pro", (err, result) => {
     if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+});
+
+router.post("/updateAdd", (req, res) => {
+  const { id, title, content, price, sono, art } = req.body;
+
+  let s;
+  if (sono === true) {
+    s = 1;
+  } else {
+    s = 0;
+  }
+
+  console.log(sono);
+
+  const sql =
+    "UPDATE ad_pro SET title_ad_pro = ?, content_ad_pro = ?, price_ad_pro = ?, sono = ?, number_art = ? WHERE id_ad_pro = ?";
+  const value = [title, content, price, s, art, id];
+
+  connection.query(sql, value, (err, result) => {
+    if (err) throw err;
+    console.log("Add Updated");
+    res.send(JSON.stringify(result));
+  });
+});
+
+router.get("/deleteAdd", (req, res) => {
+  const id = req.query.id
+
+  console.log(req.query.id);
+
+  const sql = "DELETE FROM ad_pro WHERE id_ad_pro = ?"
+  const value = id
+
+  connection.query(sql, value, (err, result) => {
+    if (err) throw err;
+    console.log('Add Deleted');
     res.send(JSON.stringify(result))
   })
 })
+
 module.exports = router;
