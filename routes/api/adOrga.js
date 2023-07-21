48b1b72647e76c33309331b2c3fec694ba9e7378
Funkinsz <fl.crespel@gmail.com>
2023-07-21 16:21:59 +0200

    fix: update ad_pro inner join user

--- routes/api/adOrga.js
+++ routes/api/adOrga.js
@@ -1,16 +1,16 @@
-const connection = require("../../database/index")
-const router = require("express").Router()
+const connection = require("../../database/index");
+const router = require("express").Router();
 
 // READ AD_ORGA
 router.get("/readAdOrga", (req, res) => {
-    const sql = 'SELECT * FROM ad_pro'
+  const sql = "SELECT * FROM `ad_pro` INNER JOIN user ON ad_pro.id_user = user.id_user";
 
-    connection.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        console.log("Récupération des prestation organisateur réussit");
-        res.send(JSON.stringify(result))
-    })
-})
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    console.log("Récupération des prestation organisateur réussit");
+    res.send(JSON.stringify(result));
+  });
+});
 
-module.exports = router
+module.exports = router;
